190579f9430e3cc28ae5a5c09110f24a52ed3d41
gitknkn <atio.asu@gmail.com>
2020-04-13 21:24:18 +0900

    [finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,15 +16,26 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "successfully"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
+    
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def destroy
